586e8c8c9f43d62474374e262a2f29dba57eb620
Jack Milton <jmilton2000@comcast.net>
2017-05-19 13:43:13 -0700

    fixed some stuff

--- save/SolarSystem.sol
+++ save/SolarSystem.sol
@@ -1,3 +1,4 @@
+<<<<<<< 1d92215ea7b057313c185d77a2ce42a969139f2a
 3
 80
 planet_1
@@ -12,3 +13,23 @@ planet_2
 1
 1
 40
+=======
+4
+80
+planet 1
+1
+1
+255
+planet 2
+1
+1
+385
+planet 3
+1
+1
+515
+planet 4
+1
+1
+645
+>>>>>>> fixed some stuff
