6f3c1a50c8a315bbcddf79974320502ad48b2b75
Jack Milton <jmilton2000@comcast.net>
2017-05-24 14:13:08 -0700

    bug fixes

--- src/Solar.java
+++ src/Solar.java
@@ -126,7 +126,7 @@ public class Solar {
     public void rename() {
         for (int A = 0; A < planets.size(); A++) {
             if (planets.get(A).getName().contains("planet")) {
-                planets.get(A).setName("planet" + A);
+                planets.get(A).setName("planet " + A + 1);
             }
         }
     }
