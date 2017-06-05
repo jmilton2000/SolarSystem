93cda61e4a7b7c90a92cbbcdd09dc3471e130c08
Jack Milton <jmilton2000@comcast.net>
2017-06-05 13:51:28 -0700

    fixed Planet warnings

--- src/Planet.java
+++ src/Planet.java
@@ -1,35 +1,49 @@
-public class Planet {
-    private String name; //defalt is Planet
-    private int size; //size are 30, 40, 50, 60 (defalt = 30)
-    private boolean hasLife; //based on distance and sun
-    private int distance; //for now = 20
+class Planet {
+    private String name; //default is Planet
+    private int size; //size are 30, 40, 50, 60 (default = 30)
+    private int distance;
     private String color;
 
-    public Planet() {
-        hasLife = false;
+    private Planet() {
         name = "Planet";
-        color = "R"; //this may be changed later
+        color = "R";
         size = 30;
     }
 
-    //geters and seters
-    public String getName() { return name; }
-    public int getSize() { return (size - 20 )/ 10; }
-    public int getSizeReal() { return size; }
-    public boolean getHasLife() { return hasLife; }
-    public int getDistance() { return distance; }
+    //getters and setters
+    String getName() {
+        return name;
+    }
 
-    public String getColor() {
+    int getSize() {
+        return (size - 20) / 10;
+    }
+
+    int getSizeReal() {
+        return size;
+    }
+
+    int getDistance() {
+        return distance;
+    }
+
+    String getColor() {
         return color;
     }
 
-    public void setName(String name) { this.name = name; }
-    public void setSize(int size) { this.size = 20 + (size * 10); }
-    public void setHasLife(boolean hasLife) { this.hasLife = hasLife; }
-    public void setDistance(int distance) { this.distance = distance; }
-
-    public void setColor(String color) {
-        this.color = color;
+    void setName(String name) {
+        this.name = name;
     }
 
+    void setSize(int size) {
+        this.size = 20 + (size * 10);
+    }
+
+    void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
 }
