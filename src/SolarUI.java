ce4fd250e6339b7096e612741be22da2a5fc7140
Jack Milton <jmilton2000@comcast.net>
2017-06-15 16:11:44 -0700

    this if the verison to turn in

--- src/SolarUI.java
+++ src/SolarUI.java
@@ -22,7 +22,7 @@ public class SolarUI extends JFrame implements ActionListener {
     private JFrame f = new JFrame();
     private Color[] colors = new Color[]{Color.RED, Color.GREEN, Color.BLUE, Color.decode("#F39C12"), Color.CYAN, Color.WHITE, Color.decode("#8E44AD")};
     private String[] help = new String[]{"Red", "Green", "Blue", "Orange", "Cyan", "White", "Purple"};
-    private ImageIcon img = new javax.swing.ImageIcon(getClass().getResource("Icon.png"));
+    private ImageIcon img = new javax.swing.ImageIcon("src/Icon.png");
     private static String OS = System.getProperty("os.name").toLowerCase();
 
     public static void main(String[] args) {
